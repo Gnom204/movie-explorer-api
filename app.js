799c1fc538de6dd011d2ab3e5bb7dc343388c111
Aleksandr Sitnikov <gnomek100500@gmail.com>
2023-07-26 13:56:03 +0300

    hotfix: исправить ошибки

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errorLogger } = require('express-winston');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const { PORT = 3000 } = require('./config');
+const { PORT, MONGO_URL } = require('./config');
 const router = require('./routes');
 const { requestLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-err');
 const { HTTP_STATUS_NOT_FOUND } = require('./utils/constants');
 const errorHandler = require('./middlewares/errorHandler');
 const allowedCors = require('./middlewares/corsProtect');
+const limiter = require('./utils/limiter');
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/diplomdb');
+mongoose.connect(MONGO_URL);
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
